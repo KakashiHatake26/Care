706f748237e8c21e6c35380a42afffe85164f1f2
KakashiHatake26 <kakashithecopyninja21@gmail.com>
2025-02-27 17:14:07 +0530

    Care-1

--- Care/CareSupreme.spec.ts
+++ Care/CareSupreme.spec.ts
@@ -42,7 +42,8 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'PAN Number' }).type('itfpk1141g');
   await page.getByRole('textbox', { name: 'DOB (DD/MM/YYYY)' }).click();
   await page.getByRole('textbox', { name: 'DOB (DD/MM/YYYY)' }).type('02/06/2001');
-  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.waitForTimeout(1000);
+  await page.locator('span.mat-button-wrapper:has-text("Submit")').nth(0).click();
   await page.getByText('Other', { exact: true }).click();
   await page.getByLabel('Identity Proof Type').getByText('Identity Proof Type').click();
   await page.getByText('PAN', { exact: true }).click();
